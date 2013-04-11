b39b6ec2a3dc68617f02fb6bda0de4585ec95e5d
Todd Wolfson <todd@twolfson.com>
2013-04-11 02:01:55 -0700

    Repaired IE6 error -- first one is passing =D Now onto appending CSS stylesheet

--- test/computedStyle_test.js
+++ test/computedStyle_test.js
@@ -48,7 +48,7 @@ describe('computedStyle', function () {
       // Color varies from browser to browser. jQuery doesn't tweak it and if we are keeping this single purpose, neithe will I.
       var color = this.color;
       assert(color);
-      assertMatches(color, /^#FF0000|rgb\(255, 0, 0\)$/);
+      assertMatches(color, /^#FF0000|rgb\(255, 0, 0\)$/i);
     });
   });
 
@@ -88,7 +88,7 @@ describe('computedStyle', function () {
     it('can find the styles', function () {
       var color = this.color;
       assert(color);
-      assertMatches(color, /^#00FF00|rgb\(0, 255, 0\)$/);
+      assertMatches(color, /^#00FF00|rgb\(0, 255, 0\)$/i);
     });
   });
 });
