e89690e272ca699bddb100c834077f733ee58f1c
Todd Wolfson <todd@twolfson.com>
2013-04-10 23:54:25 -0700

    Filling out test skeleton

--- test/computedStyle_test.js
+++ test/computedStyle_test.js
@@ -1,5 +1,11 @@
-describe('One', function () {
-  it('is one', function () {
-    // true
+describe('computedStyle', function () {
+  describe('querying an inline styled DOM element', function () {
+    it('can find the styles', function () {
+    });
+  });
+
+  describe('querying an stylesheet styled DOM element', function () {
+    it('can find the styles', function () {
+    });
   });
 });
