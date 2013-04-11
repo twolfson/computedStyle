1efd0d50d4c72c555bbe0f1a2e0b29af271d5547
Todd Wolfson <todd@twolfson.com>
2013-04-11 00:24:18 -0700

    Stylesheet on <head> seems to work for now

--- test/computedStyle_test.js
+++ test/computedStyle_test.js
@@ -37,7 +37,35 @@ describe('computedStyle', function () {
   });
 
   describe('querying an stylesheet styled DOM element', function () {
+    before(function () {
+      // Create an element
+      var el = document.createElement('div');
+      el.setAttribute('id', 'test-el');
+
+      // Save the element for later
+      this.el = el;
+
+      // Append it to the DOM
+      body.appendChild(el);
+
+      // Create a stylesheet and append it to the DOM
+      // TODO: Try out createElement, whatever quirks mode says, and doc.write
+      var stylesheet = document.createElement('style');
+      stylesheet.innerHTML = '#test-el { color: #00FF00; }';
+      window.document.documentElement.childNodes[0].appendChild(stylesheet);
+
+      // Query the element for its styles
+      var color = computedStyle(el, 'color');
+      this.color = color;
+    });
+
+    after(function () {
+      // Clean up the element
+      body.removeChild(this.el);
+    });
+
     it('can find the styles', function () {
+      assertEqual(this.color, '00FF00');
     });
   });
 });
