4c84424171e655954fc0c95a2a2f77873eb87885
Todd Wolfson <todd@twolfson.com>
2013-04-11 22:45:48 -0700

    Successfully building 140 byte version -- I will show you in a sec

--- grunt.js
+++ grunt.js
@@ -4,6 +4,7 @@ module.exports = function(grunt) {
   // Project configuration.
   grunt.initConfig({
     // Trim out comments and whitespace
+    // DEV: Uglify doesn't like partial JS scripts so this would fail
     'jsmin-sourcemap': {
       computedStyle: {
         src: 'lib/computedStyle.js',
@@ -17,6 +18,23 @@ module.exports = function(grunt) {
         src: 'tmp/computedStyle.comment_free.js',
         dest: 'tmp/computedStyle.140.js',
         replacements: [{
+          // Remove sourcemap comment
+          from: /\/\/.*/,
+          to: ''
+        }, {
+          // Remove line breaks
+          from: /\n/g,
+          to: ''
+        }, {
+          // Various word compressions
+          from: /el|prop|word|letter/g,
+          to: function (word) {
+            return word.charAt(0);
+          }
+        }, {
+          // Deal with getComputedStyle individually due to localization
+          from: /([^\.])getComputedStyle/g,
+          to: '$1g'
         }]
       }
     },
